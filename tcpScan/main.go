a43440c9a7d57213cfb87bf7455be7a9ae07503b
d <d.zidarov@gmail.com>
2024-07-07 10:17:33 -0400

    go routines with wait groups

--- tcpScan/main.go
+++ tcpScan/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 func main() {
- tooFast()
+	waitGroup()
 }
 
-func iterScanner(){
+func iterScanner() {
 	for i := 1; i < 1025; i++ {
 		address := fmt.Sprintf("scanme.nmap.org:%d", i)
 		conn, err := net.Dial("tcp", address)
@@ -22,17 +23,35 @@ func iterScanner(){
 	}
 }
 
-func tooFast(){
-  for i := 1; i < 1025; i++ {
-    go func(j int){
-     address := fmt.Sprintf("scanme.nmap.org:%d", j)
-		 conn, err := net.Dial("tcp", address)
+func tooFast() {
+	for i := 1; i < 1025; i++ {
+		go func(j int) {
+			address := fmt.Sprintf("scanme.nmap.org:%d", j)
+			conn, err := net.Dial("tcp", address)
 			if err != nil {
-   return
+				return
 			}
 			conn.Close()
 			fmt.Println(j, "open")
 		}(i)
 	}
+}
 
+func waitGroup() {
+	var wg sync.WaitGroup
+	for i := 1; i < 1025; i++ {
+		wg.Add(1)
+		go func(j int) {
+			defer wg.Done()
+			address := fmt.Sprintf("scanme.nmap.org:%d", j)
+			conn, err := net.Dial("tcp", address)
+			if err != nil {
+				fmt.Print(".")
+				return
+			}
+			conn.Close()
+			fmt.Println("\n", j, "open")
+		}(i)
+	}
+	wg.Wait()
 }
