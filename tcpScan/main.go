6266a94aaf1ac53d59ae7c2a955c17edd1b50537
d <d.zidarov@gmail.com>
2024-07-07 10:08:48 -0400

    first scanner

--- tcpScan/main.go
+++ tcpScan/main.go
@@ -1,13 +1,38 @@
 package main
 
-import(
-"fmt"
+import (
+	"fmt"
 	"net"
 )
 
-func main () {
- _, err := net.Dial("tcp", "scanme.nmap.org:80")
-	if err == nil {
-   fmt.Println("Connection successful")
+func main() {
+ tooFast()
+}
+
+func iterScanner(){
+	for i := 1; i < 1025; i++ {
+		address := fmt.Sprintf("scanme.nmap.org:%d", i)
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			// port closed or filtered
+			continue
+		}
+		conn.Close()
+		fmt.Println(i, "open")
 	}
+}
+
+func tooFast(){
+  for i := 1; i < 1025; i++ {
+    go func(j int){
+     address := fmt.Sprintf("scanme.nmap.org:%d", j)
+		 conn, err := net.Dial("tcp", address)
+			if err != nil {
+   return
+			}
+			conn.Close()
+			fmt.Println(j, "open")
+		}(i)
+	}
+
 }
